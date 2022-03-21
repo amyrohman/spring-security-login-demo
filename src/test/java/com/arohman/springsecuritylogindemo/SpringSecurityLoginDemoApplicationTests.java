3684382862aa48e9fcc38a452a3652cd45627ebe
amyrohman <arohman@mastercontrol.com>
2022-03-21 16:20:42 -0600

    Initial commit plus application.yml with JPA config

---
+++ src/test/java/com/arohman/springsecuritylogindemo/SpringSecurityLoginDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arohman.springsecuritylogindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLoginDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
