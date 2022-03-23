0480a77ec033a2f04bd254ef596155b7c27367c0
amyrohman <arohman@mastercontrol.com>
2022-03-22 18:17:20 -0600

    Saving user in db

--- src/main/java/com/arohman/springsecuritylogindemo/registration/RegistrationService.java
+++ src/main/java/com/arohman/springsecuritylogindemo/registration/RegistrationService.java
@@ -1,10 +1,31 @@
 package com.arohman.springsecuritylogindemo.registration;
 
+import com.arohman.springsecuritylogindemo.appuser.AppUser;
+import com.arohman.springsecuritylogindemo.appuser.AppUserRole;
+import com.arohman.springsecuritylogindemo.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+
+        if (!isValidEmail) {
+            throw new IllegalStateException("Email isn't valid");
+        }
+
+        return appUserService.signUpUser(new AppUser(
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail(),
+                request.getPassword(),
+                AppUserRole.USER
+        ));
     }
 }
